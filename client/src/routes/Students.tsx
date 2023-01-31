7e2702d1ca9664383002da9b1d8f9446f4411703
Lucas McDonald <l.mcdonald@medatech.ca>
2023-01-31 18:33:32 -0500

    added student POST and GET routes to server, added fetching to client

--- client/src/routes/Students.tsx
+++ client/src/routes/Students.tsx
@@ -1,8 +1,52 @@
 import NavBar from "../components/SideNav";
+import { useState } from "react";
+import { json } from "react-router-dom";
+
+type Student = {
+  firstName: string;
+  familyName: string;
+  dateOfBirth: Date;
+  id?: number;
+}
+
+type StudentsResponse = {
+  students?: Student[];
+  err?: any;
+}
 
 export default function Students() {
+  const [students, setStudents] = useState<Student[]>([]);
   return <>
-    <NavBar/>
-    <div className="page-content">Students page</div>
+    <NavBar />
+    <div className="page-content">
+      <div>
+        <button
+          onClick={async () => {
+            let req = await fetch("http://localhost:5000/students");
+            let body: StudentsResponse = await req.json();
+            if (body.err) {
+              console.log(body.err);
+            } else {
+              setStudents(body.students ?? []);
+            }
+          }}
+          type="submit"
+        >Get Students</button>
+        <button
+          onClick={async () => {
+            let student: Student = {
+              firstName: "test",
+              familyName: "student",
+              dateOfBirth: new Date()
+            }
+            let res = await fetch("http://localhost:5000/students", { method: "post", headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(student) });
+            console.log(res)
+          }}
+          type="submit"
+        >Add student</button>
+      </div>
+      <div>{JSON.stringify(students, null, 2)}</div>
+
+    </div>
   </>
 }
