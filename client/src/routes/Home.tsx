48101aa4d0d404535d40afad1c367297c1bcd6a8
Lucas McDonald <l.mcdonald@medatech.ca>
2023-02-01 18:08:06 -0500

    added home page with nav cards, basic form validation and students page styles

--- client/src/routes/Home.tsx
+++ client/src/routes/Home.tsx
@@ -1,11 +1,38 @@
 import NavBar from "../components/SideNav";
+import { Link } from "react-router-dom";
 
 
 export default function Home() {
   return <>
-    <NavBar/>
+    <NavBar />
     <div className="page-content">
-      Home page
+      <h2>Welcome</h2>
+      <div className="card-grid">
+        <Link className="card-link" to="/students">
+          <div className="card-header">
+            Students
+          </div>
+          <div className="card-body">
+            Add and view students
+          </div>
+        </Link>
+        <Link className="card-link" to="/courses">
+          <div className="card-header">
+            Courses
+          </div>
+          <div className="card-body">
+            Add and view courses
+          </div>
+        </Link>
+        <Link className="card-link" to="/results">
+          <div className="card-header">
+            Results
+          </div>
+          <div className="card-body">
+            Add and view results for students/classes
+          </div>
+        </Link>
+      </div>
     </div>
   </>
 }
--- client/src/routes/Students.tsx
+++ client/src/routes/Students.tsx
@@ -14,18 +14,14 @@ type StudentsResponse = {
   err?: any;
 }
 
-const handleStudentSubmit = async (firstName: string, familyName: string, dateOfBirth: string) => {
-  let student: Student = {
-    firstName: firstName,
-    familyName: familyName,
-    dateOfBirth: new Date(dateOfBirth + "T00:00:00.000Z")
+const isYearsApart = (firstDate: Date, secondDate: Date, years: number) => {
+  const offsetDate = new Date(firstDate);
+  offsetDate.setFullYear(firstDate.getFullYear() + years);
+  if (secondDate > offsetDate) {
+    return true;
+  } else {
+    return false;
   }
-  let res = await fetch("http://localhost:5000/students", {
-    method: "post",
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(student)
-  });
-  console.log(res);
 }
 
 export default function Students() {
@@ -33,12 +29,13 @@ export default function Students() {
   const [firstName, setFirstName] = useState("");
   const [familyName, setFamilyName] = useState("");
   const [dateOfBirth, setDateOfBirth] = useState("");
+  const [warningText, setWarningText] = useState("");
 
   useEffect(() => {
     fetch("http://localhost:5000/students").then(res => {
       res.json().then((body) => {
         if (body.err) {
-          console.log(body.err);
+          console.error(body.err);
           setStudents([]);
         } else {
           setStudents(body.students ?? []);
@@ -47,62 +44,100 @@ export default function Students() {
     });
   }, []);
 
+  const handleStudentSubmit = async (
+    event: React.FormEvent<HTMLFormElement>,
+    firstName: string,
+    familyName: string,
+    dateOfBirth: string
+  ) => {
+    let student: Student = {
+      firstName: firstName,
+      familyName: familyName,
+      dateOfBirth: new Date(dateOfBirth + "T00:00:00.000Z")
+    }
+    // Return early and display warning if student is <10
+    if (!isYearsApart(student.dateOfBirth as Date, new Date(), 10)) {
+      event.preventDefault(); // Stop page from refreshing
+      setWarningText("Student must be at least 10 years old.");
+      return;
+    }
+
+    // POST new student
+    let res = await fetch("http://localhost:5000/students", {
+      method: "post",
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(student)
+    });
+    let body = await res.json();
+    if (body.success) {
+      alert(`Student: ${student.firstName} ${student.familyName} has been successfully added to the database.`);
+    } else {
+      alert("Something went wrong. Please try again.");
+      event.preventDefault();
+    }
+  }
+
   return <>
     <NavBar />
     <div className="page-content">
-      <form className="create-student-form" onSubmit={() => handleStudentSubmit(firstName, familyName, dateOfBirth)}>
-        <label htmlFor="first-name">First Name</label>
-        <input
-          type="text"
-          required
-          placeholder="John"
-          id="first-name"
-          value={firstName}
-          onChange={event => {
-            setFirstName(event.target.value);
-          }}
-        />
-        <label htmlFor="family-name">Family Name</label>
-        <input
-          type="text"
-          required
-          placeholder="Doe"
-          id="family-name"
-          value={familyName}
-          onChange={event => {
-            setFamilyName(event.target.value);
-          }}
-        />
-        <label htmlFor="date-of-birth">Date of Birth</label>
-        <input
-          type="date"
-          required
-          id="date-of-birth"
-          value={dateOfBirth}
-          onChange={event => {
-            let newDate = new Date(event.target.value + "T00:00:00.000Z")
-            if (!isNaN(newDate.getTime())) {
-              setDateOfBirth(event.target.value);
-            }
-          }}
-        />
-        <button
-          type="submit"
-        >
-          Submit
-        </button>
-      </form>
+      <h2>Students</h2>
+      <div className="add-student-div">
+        <label className="form-title" htmlFor="create-student-form">Add a student</label>
+        <form id="create-student-form" className="create-student-form" onSubmit={(event) => handleStudentSubmit(event, firstName, familyName, dateOfBirth)}>
+          <label htmlFor="first-name">First Name</label>
+          <input
+            type="text"
+            required
+            placeholder="John"
+            id="first-name"
+            value={firstName}
+            onChange={event => {
+              setFirstName(event.target.value);
+            }}
+          />
+          <label htmlFor="family-name">Family Name</label>
+          <input
+            type="text"
+            required
+            placeholder="Doe"
+            id="family-name"
+            value={familyName}
+            onChange={event => {
+              setFamilyName(event.target.value);
+            }}
+          />
+          <label htmlFor="date-of-birth">Date of Birth</label>
+          <input
+            type="date"
+            required
+            id="date-of-birth"
+            value={dateOfBirth}
+            onChange={event => {
+              let newDate = new Date(event.target.value + "T00:00:00.000Z")
+              if (!isNaN(newDate.getTime())) {
+                setDateOfBirth(event.target.value);
+              }
+            }}
+          />
+          {warningText.length > 0 ? <span className="form-warning">{warningText}</span> : <br />}
+          <button className="submit-btn" type="submit">Submit</button>
+        </form>
+      </div>
       <table className="student-table">
-        <tr>
-          <th>First Name</th>
-          <th>Family Name</th>
-          <th>Date of Birth</th>
-        </tr>
-        {students.map(student => <tr key={student.id} className="student-row">
-          <td>{student.firstName}</td>
-          <td>{student.familyName}</td>
-          <td>{new Date(student.dateOfBirth).toDateString()}</td>
-        </tr>)}
+        <thead>
+          <tr>
+            <th>First Name</th>
+            <th>Family Name</th>
+            <th>Date of Birth</th>
+          </tr>
+        </thead>
+        <tbody>
+          {students.map(student => <tr key={student.id} className="student-row">
+            <td>{student.firstName}</td>
+            <td>{student.familyName}</td>
+            <td>{new Date(student.dateOfBirth).toDateString()}</td>
+          </tr>)}
+        </tbody>
       </table>
     </div>
   </>
